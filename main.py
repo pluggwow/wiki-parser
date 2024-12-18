21661bb600eaecbe5a3e2c46c468d6847c378b7a
pluggwow <mr.altunyan@mail.ru>
2024-12-18 16:57:56 +0300

    исправил некоторые ошибки

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # main - стартовый модуль проекта
 
- # импорты из модуля
+# импорты из модуля
 from parse import func1, extract_currency_data, get_response
 
 # функция запуска импортированных функций 
-    def main():
+def main():
     url = 'https://en.wikipedia.org/wiki/ISO_4217'
     table = get_response(url)
     if table:
         print(extract_currency_data(table))
     else:
         print("Не удалось получить данные.")
+
+# Вызов функции main для запуска программы
+if __name__ == "__main__":
+    main()
