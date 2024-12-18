a645d5a42f567c2ab71ca35cfe7b746065697d18
pluggwow <mr.altunyan@mail.ru>
2024-12-18 14:23:23 +0300

    исправил ошибки, добавил код на случай неудачи в поиске данных

--- main.py
+++ main.py
@@ -4,10 +4,10 @@
 from parse import func1, extract_currency_data, get_response
 
 # функция запуска импортированных функций 
-def main():
-    url = 'https://en.wikipedia.org/wiki/ISO_4217'
-    print(extract_currency_data(get_response(url)))
-
-# инициализонный скрипт 
-if __name__ == '__main__':
-    main()
+    def main():
+     url = 'https://en.wikipedia.org/wiki/ISO_4217'
+    table = get_response(url)
+    if table:
+        print(extract_currency_data(table))
+    else:
+        print("Не удалось получить данные.")
