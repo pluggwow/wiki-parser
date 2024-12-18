5418e4227f1a2af2cdf42b7e9637c5e409157af4
pluggwow <mr.altunyan@mail.ru>
2024-12-18 11:20:18 +0300

    добавил структуру функции

--- functions.py
+++ functions.py
@@ -1,11 +1,22 @@
 # functions - модуль для импорта функций для решения задачи
-
 # TODO - добавить импорты внешних зависимостей 
 # import <название зависимости>
 
-def func1():
-    pass
+def extract_currency_data(table):
+    # Списки для хранения данных
+    codes = []
+    nums = []
+    currencies = []
 
+    # Проходим по всем строкам таблицы, начиная со второй (пропускаем заголовок)
+    for row in table.find_all('tr')[1:]:
+        cols = row.find_all('td')
+        if len(cols) >= 3:  # Убедимся, что в строке достаточно столбцов
+            codes.append(cols[0].text.strip())  # Код валюты
+            nums.append(cols[1].text.strip())    # Номер валюты
+            currencies.append(cols[2].text.strip())  # Название валюты
 
-def func2():
-    pass
+    return codes, nums, currencies
+
+# Пример вызова функции
+# codes, nums, currencies = extract_currency_data(table)
