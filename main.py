a78b65f56951b3b1f3dd2d4191621e0cda00be91
pluggwow <mr.altunyan@mail.ru>
2024-12-18 14:26:06 +0300

    5 ошибок

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from parse import func1, extract_currency_data, get_response
 
 # функция запуска импортированных функций 
     def main():
-     url = 'https://en.wikipedia.org/wiki/ISO_4217'
+    url = 'https://en.wikipedia.org/wiki/ISO_4217'
     table = get_response(url)
     if table:
         print(extract_currency_data(table))
