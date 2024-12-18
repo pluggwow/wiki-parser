ab726bf1d485c8c82b32fa8b3a2a5ab091469d35
pluggwow <mr.altunyan@mail.ru>
2024-12-18 11:45:31 +0300

    вызвал функцию(ошибка)

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 # main - стартовый модуль проекта
 
  # импорты из модуля
-from functions import func1, func2
+from functions import func1, extract_currency_data
 
 # функция запуска импортированных функций 
-def main (): 
-    pass
+def main():
+    table = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <table>
+            <th>Artur</th>
+            <tr>Altunyan</tr>
+        </table>
+    </body>
+    </html>
+    """ 
+    print(extract_currency_data(table))
 
 # инициализонный скрипт 
- if __name__ == '__main__':
+if __name__ == '__main__':
     main()
